4441d12d24ee44f14452f8cc6febfd3fb756c339
Manoj <manojkumarmc@hotmail.com>
2014-10-13 12:16:41 +0530

    Update controllers.js

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -1,24 +1,9 @@
 myApp.controller('MyCtrl',['$scope','socket', function($scope, socket){
 
   $scope.cObjs = []
-
-  $scope.cObj = {
-    id : -1,
-    name : 'This is an Object'
-  }
-
+  
   socket.on('container:add', function(data) {
-    var found = false;
-    var cObjs = $scope.cObjs
-
-    for (var i = 0; i < cObjs.length -1; i++) {
-      if (cObj.id == data.id) {
-        found = true
-      }
-    }
-    if (!found) {
-      $scope.cObjs.push(data)
-    }
+    $scope.cObjs.push(data)
   })
 
 }])
